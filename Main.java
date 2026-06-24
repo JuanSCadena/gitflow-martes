21b90c85631a68fff6e35d0aa8cf6827bd6fcf69
JuanSCadena <js.cadena@outlook.com>
2026-06-24 12:47:32 -0500

    correccion bug

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("***************");
+        System.out.println("* JAVA PROJECT *");
+        System.out.println("***************");
+
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Mundo"));
-        feature/despedida-personalizada
         Despedida despedida = new Despedida();
         System.out.println(despedida.generarDespedida("Mundo"));
        
